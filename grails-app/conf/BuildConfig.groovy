38adc0bb60f5b91ff2d71f2acf4b044ad02ab4a6
Thiago Alves dos Santos <thiago.alv.santos@gmail.com>
2012-08-04 20:08:01 -0300

    configurando relatorio do codenarc

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,3 +49,20 @@ grails.project.dependency.resolution = {
 		build ":tomcat:$grailsVersion"
 	}
 }
+
+codenarc.reports = {
+	// Each report definition is of the form:
+	//    REPORT-NAME(REPORT-TYPE) {
+	//        PROPERTY-NAME = PROPERTY-VALUE
+	//        PROPERTY-NAME = PROPERTY-VALUE
+	//    }
+
+	MyXmlReport('xml') {                    // The report name "MyXmlReport" is user-defined; Report type is 'xml'
+		outputFile = 'target/test-reports/CodeNarcReport.xml'  // Set the 'outputFile' property of the (XML) Report
+		title = 'Titanio - Codenarc'             // Set the 'title' property of the (XML) Report
+	}
+	MyHtmlReport('html') {                  // Report type is 'html'
+		outputFile = 'target/test-reports/CodeNarcReport.html'
+		title = 'Titanio - Codenarc'
+	}
+}
